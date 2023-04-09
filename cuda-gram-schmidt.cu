fa6b1fe6668d3e551755fd2672124cdfd2d884db
Aidan McHugh <mchughaw@gmail.com>
2023-04-09 19:27:20 -0400

    Testing and fixing cuda methods

--- cuda-gram-schmidt.cu
+++ cuda-gram-schmidt.cu
@@ -2,48 +2,46 @@
 // CUDA CODE FOR MODIFIED GRAM SCHMIDT
 //
 
+#define calc1dIndex blockIdx.x *blockDim.x + threadIdx.x
 
 /*
-    * vector_subtraction
-    * @PARM n size of both arrays
-    * @PARM *x vector of minuend
-    * @PARM *y vector of subtrahend
-    * @REQUIRES *x and *y be equal in size
-    * @REQUIRES *x and *y be a pointer in device memory
-    * @MODIFIES *y
-    * @EFFECTS *y[i] is the difference x[i] - y[i]
-*/
+ * vector_subtraction
+ * @PARM n size of both arrays
+ * @PARM *x vector of minuend
+ * @PARM *y vector of subtrahend
+ * @REQUIRES *x and *y be equal in size
+ * @REQUIRES *x and *y be a pointer in device memory
+ * @MODIFIES *y
+ * @EFFECTS *y[i] is the difference x[i] - y[i]
+ */
 __global__ void vector_subtraction(int n, float *x, float *y) {
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int index = calc1dIndex;
     int stride = blockDim.x * gridDim.x;
     for (int i = index; i < n; i += stride)
         y[i] = x[i] - y[i];
 }
 
-
 /*
-    * vector_projection
-    * @PARM n size of both vectors
-    * @PARM *x vector
-    * @PARM *y vector
-    * @PARM *result a pointer to a single float value in which the result will be stored.
-    * @REQUIRES *x and *y be equal in size
-    * @REQUIRES *x and *y be a pointer in device memory
-*/
+ * vector_projection
+ * @PARM n size of both vectors
+ * @PARM *x vector
+ * @PARM *y vector
+ * @PARM *result a pointer to a single float value in which the result will be stored.
+ * @REQUIRES *x and *y be equal in size
+ * @REQUIRES *x and *y be a pointer in device memory
+ */
 __global__ void vector_dot_product(int n, float *x, float *y, float *result) {
-    __shared__ float temp[n];
-    int index = threadIdx.x + blockIdx.x * blockDim.x;
+    extern __shared__ float temp[];
+
+    int index = calc1dIndex;
     temp[index] = x[index] * y[index];
 
     __syncthreads();
 
     if (index == 0) {
         *result = 0;
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             *result += temp[i];
         }
     }
 }
-
-
