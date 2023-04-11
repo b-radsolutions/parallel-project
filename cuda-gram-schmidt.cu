c6b02b5c98e367439cd0bdc7a197989f029a9f5b
Aidan McHugh <mchughaw@gmail.com>
2023-04-11 13:43:41 -0400

    Basic test

--- cuda-gram-schmidt.cu
+++ cuda-gram-schmidt.cu
@@ -2,6 +2,8 @@
 // CUDA CODE FOR MODIFIED GRAM SCHMIDT
 //
 
+#include <cstdlib>
+
 #define calc1dIndex blockIdx.x *blockDim.x + threadIdx.x
 
 /*
@@ -11,14 +13,14 @@
  * @PARM *y vector of subtrahend
  * @REQUIRES *x and *y be equal in size
  * @REQUIRES *x and *y be a pointer in device memory
- * @MODIFIES *y
- * @EFFECTS *y[i] is the difference x[i] - y[i]
+ * @MODIFIES *x
+ * @EFFECTS *x[i] is the difference x[i] - y[i]
  */
 __global__ void vector_subtraction(int n, float *x, float *y) {
     int index = calc1dIndex;
     int stride = blockDim.x * gridDim.x;
     for (int i = index; i < n; i += stride)
-        y[i] = x[i] - y[i];
+        x[i] = x[i] - y[i];
 }
 
 /*
@@ -44,4 +46,94 @@ __global__ void vector_dot_product(int n, float *x, float *y, float *result) {
             *result += temp[i];
         }
     }
+}
+
+// ----------------------------------------
+// Cuda Entry Points
+// ----------------------------------------
+
+float *magnitude;
+
+void cudaSetup() {
+    // Set up memory to hold the result of dot product
+    cudaMalloc(&magnitude, sizeof(double));
+}
+
+void cudaCleanup() { cudaFree(magnitude); }
+void cleanupMatrix(double **A, size_t m) {
+    for (size_t i = 0; i < m; i++)
+        cudaFree(A[i]);
+    free(A);
+}
+
+// Create 'n' random columns of 'n' entries
+double **createTestMatrix(size_t n) {
+    double **ret, *tmp, *local;
+
+    // Create local to hold the randomly-generated column
+    local = (double *)malloc(sizeof(double) * n);
+
+    // ret will be created on the CPU so it can reference the devices pointers
+    ret = (double **)malloc(sizeof(double *) * n);
+
+    for (size_t i = 0; i < n; i++) {
+        // Randomly populate local copy
+        for (size_t j = 0; j < n; j++) {
+            local[j] = ((double)rand() / (double)RAND_MAX);
+        }
+        // Transfer local copy onto the device
+        cudaMalloc(&tmp, sizeof(double) * n);
+        cudaMemcpy(tmp, local, sizeof(double) * n, cudaMemcpyHostToDevice);
+        // Set the row
+        ret[i] = tmp;
+    }
+
+    free(local);
+
+    return ret;
+}
+
+double **allocateMatrix(size_t n) {
+    double **ret, *tmp;
+    // ret will be created on the CPU so it can reference the device pointers
+    ret = (double **)malloc(sizeof(double *) * n);
+    for (size_t i = 0; i < n; i++) {
+        // Transfer local copy onto the device
+        cudaMalloc(&tmp, sizeof(double) * n);
+        cudaMemcpy(tmp, local, sizeof(double) * n, cudaMemcpyHostToDevice);
+        // Set the row
+        ret[i] = tmp;
+    }
+    return ret;
+}
+
+void normalize(double *src, double *dst, size_t n) {
+    // Find the value to divide by
+    vector_dot_product<<<1, n, sizeof(double) * n>>>(n, src, src, magnitude);
+    if (src != dst) {
+        // Need to copy the src into the dst before we divide
+        cudaMemcpy(dst, src, sizeof(double) * n, cudaMemcpyDeviceToDevice);
+    }
+    // Divide happens here
+    // todo:: add the divide
+}
+
+// Requires the base to have magnitude 1 (to avoid an extra dot product)
+void projection(double *vector, double *base, double *result, size_t n) {
+    // Find the numerator for the projection quotient
+    vector_dot_product<<<1, n, sizeof(double) * n>>>(n, src, src, magnitude);
+    // We assume the base to have magnitude 1, saving us from this division
+    if (base != result) {
+        // Need to copy the base to the result before we multiply, as it happens in-place.
+        cudaMemcpy(result, base, sizeof(double) * n, cudaMemcpyDeviceToDevice);
+    }
+    // Now, we can multiply the base by this magnitude
+    // todo:: add the multiplication
+}
+
+void subtract(double *a, double *b, double *dst, size_t n) {
+    if (a != dst) {
+        cudaMemcpy(dst, a, sizeof(double) * n, cudaMemcpyDeviceToDevice);
+    }
+    vector_subtraction<<<1, n, sizeof(double) * n>>>(n, dst, b);
 }
