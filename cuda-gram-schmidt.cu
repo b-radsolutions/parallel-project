c6b96af2e05259c6682a4c4449bfa83a6cedd1a0
Aidan McHugh <mchughaw@gmail.com>
2023-04-22 12:48:37 -0400

    First pass at parallel gram-schmidt

--- cuda-gram-schmidt.cu
+++ cuda-gram-schmidt.cu
@@ -188,3 +188,64 @@ void subtract(double *a, double *b, double *dst, size_t n) {
     }
     vector_subtraction<<<1, n>>>(n, dst, b);
 }
+
+/**
+ * Subtract the base vector from every other vector using the magnitude in scalars for the
+ * base vector.
+ * V = V - CB where v = vectors, b = base, and C is scalars.
+ * len(result) == len(vectors) == len(scalars) == num_vectors ;
+ * len(vectors[:]) == len(base) == n
+ */
+__global__ void many_vector_subtractions(double *base, double **vectors, double *scalars,
+                                         size_t num_vectors, size_t n) {
+
+    int    index = calc1dIndex;
+    int    stride = blockDim.x * gridDim.x;
+    size_t total = num_vectors * n;
+
+    for (int i = index; i < total; i += stride) {
+        int block = i / n;
+        int vi = i % n;
+        vectors[block][vi] -= base[vi] * scalars[block];
+    }
+}
+
+/**
+ * Dot the base with every other vector, returning the individual dot products in result.
+ * len(result) == len(vectors) == num_vectors ;
+ * len(vectors[:]) == len(base) == n
+ */
+__global__ void many_vector_dot_product(double *base, double **vectors,
+                                        size_t num_vectors, size_t n, double *result) {
+    extern __shared__ double temp[];
+
+    int    index = calc1dIndex;
+    int    stride = blockDim.x * gridDim.x;
+    size_t total = num_vectors * n;
+
+    for (int i = index; i < total; i += stride) {
+        int block = i / n;
+        int vi = i % n;
+        temp[block] += vectors[block][vi] * base[vi];
+    }
+}
+
+// Removes the projection of the completed index from every vector afterwards. A
+// has `m` columns and `n` rows.
+void performModifiedGramSchmidtReduction(double **A, size_t m, size_t n,
+                                         size_t completed_index) {
+    double  *base = A[completed_index];
+    size_t   remainder_count = m - (completed_index + 1);
+    double **remainder = (A + completed_index + 1);
+    size_t   coefficient_size = sizeof(double) * remainder_count;
+    double  *dots;
+    cudaMalloc(&dots, coefficient_size);
+    // many_vector_projection_subtractions<<<1, n>>>(base, remainder, remainder_count, n);
+    // Do all of the dot products
+    many_vector_dot_product<<<1, n, coefficient_size>>>(base, remainder, remainder_count,
+                                                        n, dots);
+    // Communicate with the other MPI ranks to discover the complete dot product
+    // todo:: incorporate MPI
+    // Use that dot product to do vector subtractions
+    many_vector_subtractions<<<1, n>>>(base, remainder, dots, remainder_count, n);
+}
