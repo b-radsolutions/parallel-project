dcba176bec9f5e7fc56879618d996f51666bcb05
Aidan McHugh <mchughaw@gmail.com>
2023-04-22 19:47:31 -0400

    Modified Gram-Schmidt's complete

--- modified-gram-schmidt.cpp
+++ modified-gram-schmidt.cpp
@@ -8,7 +8,7 @@
 
 // For an m x n matrix A (A[m][n] is the bottom right entry, A has m columns with n rows
 // each), orthonormalize the matrix A and put the result in the pre-allocated Q.
-void modified_gram_schmidt(double **A, size_t m, size_t n, double **Q) {
+void serial_modified_gram_schmidt(double **A, size_t m, size_t n, double **Q) {
     // Copy over A into the output Q
     matrixCopy(A, Q, m, n);
 
@@ -30,28 +30,43 @@ void modified_gram_schmidt(double **A, size_t m, size_t n, double **Q) {
     free(tmp);
 }
 
-int main() {
-    cudaSetup();
-    printf("cudaSetup() complete\n");
+void parallel_modified_gram_schmidt(double **A, size_t m, size_t n, double **Q) {
+    // Copy over A into the output Q
+    matrixCopy(A, Q, m, n);
 
-    // Create the matrix to use.
-    srand(0);
-    const size_t n = 10;
-    double     **A, **Q;
-    A = createTestMatrix(n);
-    printf("Matrix A (%dx%d) successfully generated\n", n, n);
-    Q = allocateMatrix(n);
-    printf("Matrix Q (%dx%d) successfully initialized\n", n, n);
+    // Our first vector is already done
+    normalize(Q[0], Q[0], n);
 
-    // Run the procedure
-    modified_gram_schmidt(A, n, n, Q);
-    printf("Modified gram schmidt complete\n", n, n);
-
-    // Cleanup
-    cleanupMatrix(A, n);
-    printf("cleanupMatrix(A) complete\n");
-    cleanupMatrix(Q, n);
-    printf("cleanupMatrix(Q) complete\n");
-    cudaCleanup();
-    printf("cudaCleanup() complete\n");
+    for (size_t i = 0; i < m - 1; i++) {
+        // Perform the 1-op projection/subtraction on remaining vectors
+        performModifiedGramSchmidtReduction(A, m, n, i);
+        // Normalize the vector we just completed
+        normalize(Q[i + 1], Q[i + 1], n);
+    }
 }
+
+// int main() {
+//     cudaSetup();
+//     printf("cudaSetup() complete\n");
+
+//     // Create the matrix to use.
+//     srand(0);
+//     const size_t n = 10;
+//     double     **A, **Q;
+//     A = createTestMatrix(n);
+//     printf("Matrix A (%dx%d) successfully generated\n", n, n);
+//     Q = allocateMatrix(n);
+//     printf("Matrix Q (%dx%d) successfully initialized\n", n, n);
+
+//     // Run the procedure
+//     modified_gram_schmidt(A, n, n, Q);
+//     printf("Modified gram schmidt complete\n", n, n);
+
+//     // Cleanup
+//     cleanupMatrix(A, n);
+//     printf("cleanupMatrix(A) complete\n");
+//     cleanupMatrix(Q, n);
+//     printf("cleanupMatrix(Q) complete\n");
+//     cudaCleanup();
+//     printf("cudaCleanup() complete\n");
+// }
