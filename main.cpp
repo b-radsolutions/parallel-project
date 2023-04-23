4db3bf2c807f9bb0d4c00fea0a8a327fe7b57f25
Aidan McHugh <mchughaw@gmail.com>
2023-04-23 19:33:47 -0400

    Needed changes

--- main.cpp
+++ main.cpp
@@ -11,16 +11,12 @@
 #include "tools/read_matrix_mpi.cpp"
 #include "tools/read_matrix_serial.cpp"
 
-#ifndef BRAD
 #include "clockcycle.h"
-#else
-int clock_now() { return 0; }
-#endif
 
 #define MASTER 0
 #define clock_frequency 512000000.0
 
-#define NUM_SIZES 8
+#define NUM_SIZES 11
 #define NUM_MATRIX_VARAINTS 4
 
 using namespace std;
@@ -45,86 +41,8 @@ int main(int argc, char *argv[]) {
     // ranks create the helper structures locally.
     cudaSetup(world_rank);
 
-    int         sizes[8] = {4, 16, 32, 64, 128, 256, 512, 1024};
+    int         sizes[11] = {4, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192};
     std::string types[4] = {"dense", "sparse", "well-conditioned", "ill-conditioned"};
-    if (world_rank == MASTER) {
-        printf("MPI Rank 0: ------Running Serial------\n");
-        for (int i = 0; i < NUM_SIZES; i++) {
-            int n = sizes[i];
-            for (int j = 0; j < NUM_MATRIX_VARAINTS; j++) {
-                string in_filename = "data/" + types[j] + "/" + to_string(n) + ".mtx";
-                cout << "GOING TO Matrix " << in_filename << "\t" << n << " by " << n
-                     << "in Serial\n ";
-                start = clock_now();
-                double **input_matrix = read_matrix(in_filename, n);
-                end = clock_now();
-                cout << "READ Matrix " << n << " by " << n << " in " << (end - start)
-                     << " cycles (" << (end - start) / clock_frequency << " secs)\n";
-
-                // Need to move the matrix to the device...
-                double **A = matrixHostToDevice(input_matrix, n, n);
-
-                cout << "RUNNING Serial Modified Gram-Schmidt\t" << types[j] << "\t" << n
-                     << "\n";
-
-                // Preallocate output
-                double **Q = allocateMatrix(n);
-
-                start = clock_now();
-                serial_modified_gram_schmidt(A, n, n, Q);
-                end = clock_now();
-
-                cout << "DONE in " << (end - start) << " cycles ("
-                     << (end - start) / clock_frequency << " secs)\n";
-
-                // Matrix Q is a device matrix and needs to be moved
-                double **deviceQ = matrixDeviceToHost(Q, n, n);
-
-                start = clock_now();
-                string out_filename = "out/ModifiedSerial" + to_string(n) + "by" +
-                                      to_string(n) + types[j] + ".mtx";
-                write_matrix_to_file_serial(deviceQ, n, out_filename);
-                end = clock_now();
-                cout << "WROTE TO FILE in " << (end - start) << "cycles("
-                     << (end - start) / clock_frequency << " secs)\n ";
-
-                // Delete host copy of Q.
-                for (size_t i = 0; i < n; i++)
-                    free(deviceQ[i]);
-                free(deviceQ);
-
-                cout << "RUNNING Serial Classic Gram-Schmidt\t" << types[j] << "\t" << n
-                     << "\n";
-
-                start = clock_now();
-                normal_gram_schmidt(A, n, n, Q);
-                end = clock_now();
-
-                cout << "DONE in " << (end - start) << " cycles ("
-                     << (end - start) / clock_frequency << " secs)\n";
-
-                // Matrix Q is (still) a device matrix and needs to be moved
-                deviceQ = matrixDeviceToHost(Q, n, n);
-
-                start = clock_now();
-                out_filename = "out/ClassicSerial" + to_string(n) + "by" + to_string(n) +
-                               types[j] + ".mtx";
-                write_matrix_to_file_serial(deviceQ, n, out_filename);
-                end = clock_now();
-                cout << "WROTE TO FILE in " << (end - start) << " cycles ("
-                     << (end - start) / clock_frequency << " secs)\n\n";
-
-                // Delete host copy of Q for classic GS.
-                for (size_t i = 0; i < n; i++)
-                    free(deviceQ[i]);
-                free(deviceQ);
-
-                // Delete A
-                cleanupMatrix(A, n);
-            }
-            cout << "\n";
-        }
-    }
 
     // MPI portion
 
@@ -204,7 +122,6 @@ int main(int argc, char *argv[]) {
                 free(B);
             }
 
-            // write_partial_matrix(deviceQ, n, first_row, m, out_filename);
             end = clock_now();
             MPI_Barrier(MPI_COMM_WORLD);
 
