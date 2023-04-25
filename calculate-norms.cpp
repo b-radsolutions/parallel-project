7a060d5fabe44dc543cc77b310c73d248c3c605c
Avery Iorio <24ioria@gmail.com>
2023-04-25 13:53:07 -0400

    new ortho

--- calculate-norms.cpp
+++ calculate-norms.cpp
@@ -11,29 +11,31 @@
 #include "orthogonality-test.hpp"
 #include "tools/read_matrix_serial.hpp"
 
-void calculateError(std::string filename, std::string fold) {
+
+void calculateError(std::string filename, std::string fold)
+{
     char str1[100] = "";
     strcat(str1, fold.c_str());
     strcat(str1, filename.c_str());
     std::ifstream file(str1, std::ios::in | std::ios::binary);
     size_t m;
     file.read((char *)(&m), sizeof(size_t));
-
+    printf("%s \n", filename.c_str());
     double **Q, **E;
     double frob_norm, one_norm, inf_norm;
-
+    char bad = '.';
+    if((filename.c_str())[0] == bad) {
+        return;
+    }
     Q = read_matrix(str1, m);
     E = orthoError(m, m, Q);
     frob_norm = frobeniusNorm(m, m, E);
     one_norm = oneNorm(m, m, E);
     inf_norm = infNorm(m, m, E);
-    printf("%lu %lf %lf %lf %s\n", m, frob_norm, one_norm, inf_norm, filename.c_str());
+    printf("%lu, %lf, %lf, %lf, %s\n", m, frob_norm, one_norm, inf_norm, filename.c_str());
 }
 
-//either accepts a directory if flag -r is provided before the name of the directory
-//or accepts a single file
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
     printf("%s\n", argv[1]);
     if(strcmp(argv[1], "-r") == 0) {
         printf("%s\n", argv[2]);
@@ -58,4 +60,5 @@ int main(int argc, char *argv[])
         calculateError(argv[1], "");
     }
     return 0;
+
 }
